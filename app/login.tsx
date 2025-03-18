287fc4bfb8aac8af257c5fd996cde12f7b04c4f6
math3wsl3vi <levimathews9841@gmail.com>
2025-03-18 12:06:33 +0300

    final

--- app/login.tsx
+++ app/login.tsx
@@ -7,11 +7,11 @@ const LoginScreen = () => {
   return (
     <View>
       <View className='flex items-center mt-20'>
-        <Image source={require('./../assets/login.png')} className='w-[230px] h-[450px] rounded-md'/>
+        <Image source={require('./../assets/top.jpeg')} className='w-[230px] h-[450px] rounded-md'/>
       </View>
       <View className='p-[25px] bg-green-1 h-full flex items-center -mt-1 rounded-xl'>
         <Text className='text-5xl uppercase font-bold text-white text-center font-popB'>Stay on track, stay healthy!</Text>
-        <Text className='text-xl font-semibold mt-10 text-white text-center font-popSb'>Track your medication, take control of your health. Stay Consistent. Stay Ready. Gotta stay in the cold!</Text>
+        <Text className='text-xl font-semibold mt-10 text-white text-center font-popSb'>Track your medication, take control of your health. Stay Consistent. Gotta Stay Ready. Gotta stay in the cold!</Text>
         <TouchableOpacity
         onPress={()=>router.push('/SigninPage')}
         className='py-3 px-4 bg-white rounded-md w-full mt-5'>
